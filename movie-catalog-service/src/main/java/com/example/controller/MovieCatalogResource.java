a1de937b5dae2a47e7174fee17698c68ff075a7b
megatron8010 <aditya.singh8010@gmail.com>
2020-04-23 16:10:27 +0530

    added hardcoded rest api using resttemplate

--- movie-catalog-service/src/main/java/com/example/controller/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/controller/MovieCatalogResource.java
@@ -1,20 +1,39 @@
 package com.example.controller;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.entity.CatalogItem;
+import com.example.entity.Movie;
+import com.example.entity.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
-	
+	@Autowired
+    private RestTemplate restTemplate;
+
 	@RequestMapping("/{userid}")
 	public List<CatalogItem> getCatalog(@PathVariable("userid") String userid) {
-        return Collections.singletonList(new CatalogItem("Test", "Test Desc", 4));
+	//	RestTemplate restTemplate = new RestTemplate();
+	//	Movie movie = restTemplate.getForObject("localhost:8081/movies/foo", Movie.class); //parsing getting the json from the url and making it an object of movie.class
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 3),
+                new Rating("5678", 4)
+				);
+		return ratings.stream()
+				.map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList());	
     }
 }
--- movie-catalog-service/src/main/java/com/example/demo/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/example/demo/MovieCatalogServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.example.controller")
@@ -13,5 +15,8 @@ public class MovieCatalogServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
